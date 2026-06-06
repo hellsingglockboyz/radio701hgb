c376dcc8bad672c1a4d3c9476d86df37ce476084
hellsingglockboyz <vampirezarereal@gmail.com>
2026-06-06 18:35:08 -0300

    only hellsing radio
      { freq: "7.1.0", name: "HELLSING CAST", src: "assets/audio/set8_hellsingcast.mp3" },
      { freq: "5.555", name: "MINAJ STATION", src: "assets/audio/set2_robert.mp3" },
      { freq: "33.3", name: "RADIO MALDADE", src: "assets/audio/set3_shinz.mp3" },
      { freq: "97.1", name: "DJ ERA FM", src: "assets/audio/set4_djera.mp3" },
      { freq: "16.29", name: "BADASSFCKNKID RADIO", src: "assets/audio/set5_tsuu.mp3" },
      { freq: "0.800", name: "BoulNoArt FM", src: "assets/audio/set6_esdras.mp3" }
    
    removed

--- assets/js/script.js
+++ assets/js/script.js
@@ -409,12 +409,6 @@ const volumeMax = 300;
 const stations = [
   { freq: "00.0", name: "NO SIGNAL", src: null },
   { freq: "7.0.1", name: "HELLSING RADIO", src: "assets/audio/set1_701_album.mp3" },
-  { freq: "7.1.0", name: "HELLSING CAST", src: "assets/audio/set8_hellsingcast.mp3" },
-  { freq: "5.555", name: "MINAJ STATION", src: "assets/audio/set2_robert.mp3" },
-  { freq: "33.3", name: "RADIO MALDADE", src: "assets/audio/set3_shinz.mp3" },
-  { freq: "97.1", name: "DJ ERA FM", src: "assets/audio/set4_djera.mp3" },
-  { freq: "16.29", name: "BADASSFCKNKID RADIO", src: "assets/audio/set5_tsuu.mp3" },
-  { freq: "0.800", name: "BoulNoArt FM", src: "assets/audio/set6_esdras.mp3" },
 ];
 
 const stationStartTimes = stations.map(() => null);
